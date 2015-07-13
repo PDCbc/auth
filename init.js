d7c7ec69983ef427470117d704ad58a0fa4cb83f
Simon Diemert <simon.diemert@gmail.com>
2015-07-13 11:10:52 -0700

    added interaction diagrams in UML models; minor format changes in code.

--- init.js
+++ init.js
@@ -1,23 +1,26 @@
 'use strict';
 var async = require('async'),
-    _ = require('lodash'),
+    _     = require('lodash'),
     logger = require('./lib/logger');
 
 
 async.auto({
     environment: require('./lib/environment'),
     certificate: require('./lib/certificate'),
-    httpd:       require('./lib/httpd'),
-    routes:      require('./lib/routes'),
+    httpd : require('./lib/httpd'),
+    routes: require('./lib/routes')
 }, complete);
 
 /**
-* The final completion function. Throws any errors that arise, or listens.
-* @param {Error} error Any errors passed to us via `next(err, null)`` from tasks.
-* @param {Object} data The complete async data object.
-*/
+ * The final completion function. Throws any errors that arise, or listens.
+ * @param {Error} error Any errors passed to us via `next(err, null)`` from tasks.
+ * @param {Object} data The complete async data object.
+ */
 function complete(error, data) {
-    if (error) { logger.error(error); throw error; }
+    if (error) {
+        logger.error(error);
+        throw error;
+    }
     // No errors
     require('https').createServer(data.certificate, data.httpd.main)
         .listen(process.env.MAINPORT, function () {

93c559ef4124fe6b82c97d2cc176c10942cfb53b
sdiemert <sdiemert@users.noreply.github.com>
2015-10-20 12:03:22 -0700

    Added password management to manage_users.sh

--- manage_users.sh
+++ manage_users.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Takes arguments: username, clinician_id, endpoint_id, juri
+# Takes arguments: username, clinician_id, endpoint_id, juri, passwd
 
 USER=$1
 CLINICIAN=$2
 ENDPOINT=$3
 JURI=$4
+PASSWD=$5
 
-`dacspasswd -uj $JURI -pds '{"clinician":"'$CLINICIAN'", "clinic":"'$ENDPOINT'"}' -- $USER`
+echo "$PASSWD" | dacspasswd -uj $JURI -pf - -a $USER &> /dev/null
+
+`dacspasswd -uj $JURI -pds '{"clinician":"'$CLINICIAN'", "clinic":"'$ENDPOINT'"}' -- $USER

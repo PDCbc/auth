314efb377aff2509b0dd9f8e906f6963b12053b1
sdiemert <simon.diemert@gmail.com>
2015-10-21 00:19:09 +0000

    improved manage_user script

--- manage_users.sh
+++ manage_users.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-# Takes arguments: username, clinician_id, endpoint_id, juri
+# Takes arguments: username, clinician_id, endpoint_id, juri, passwd 
 
 USER=$1
 CLINICIAN=$2
 ENDPOINT=$3
 JURI=$4
+PASSWD=$5
+
+DACS_ROLEFILE=/etc/dacs/federations/pdc.dev/roles
+
+# Add user to DACS
+#
+if( dacspasswd -uj ${JURI} -l | grep ${USER})
+then
+	echo "Existing DACS user replaced"
+	/usr/bin/dacspasswd -uj ${JURI} -d ${USER}
+fi
+/usr/bin/dacspasswd -uj ${JURI} -p ${PASSWD} -a ${USER}
+
+
+
+if ( cat ${DACS_ROLEFILE} | grep -qio ^${USER}: )
+then
+        echo "Existing user role replaced"
+        sed -i /${USER}:/d ${DACS_ROLEFILE}
+fi
+
+echo ${USER}:admin | tee -a ${DACS_ROLEFILE}
 
 `dacspasswd -uj $JURI -pds '{"clinician":"'$CLINICIAN'", "clinic":"'$ENDPOINT'"}' -- $USER`
